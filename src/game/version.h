8cf9b37f4e447d4495460b4fe4a18801be48a72c
Patrick Rathje <pr@braune-digital.com>
2015-12-23 23:55:43 +0100

    merge

--- src/game/version.h
+++ src/game/version.h
@@ -5,5 +5,6 @@
 #include "generated/nethash.cpp"
 #define GAME_VERSION "0.6.4"
 #define GAME_NETVERSION "0.6 " GAME_NETVERSION_HASH
+#define MOD_VERSION "0.7.5"
 static const char GAME_RELEASE_VERSION[8] = {'0', '.', '6', '.', '4', 0};
 #endif
