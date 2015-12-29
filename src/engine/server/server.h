08a325cc6192a7c50366059d8aaa443e4d2e26b3
Patrick Rathje <git@PatrickRathje.de>
2015-12-30 00:16:05 +0100

    fix

--- src/engine/server/server.h
+++ src/engine/server/server.h
@@ -34,7 +34,7 @@ public:
 
 	CSnapIDPool();
 
-	void Reset(bool ResetAccount = true);
+	void Reset();
 	void RemoveFirstTimeout();
 	int NewID();
 	void TimeoutIDs();
@@ -131,7 +131,7 @@ public:
 		char m_aAccountName[MAX_ACCOUNT_NAME_LENGTH];
 		int64 m_LastAccountRequestTime;
 		int m_NumAccountRequests;
-		void Reset();
+		void Reset(bool ResetAccount = true);
 	};
 	
 	void AccountRequest(int ClientID);
